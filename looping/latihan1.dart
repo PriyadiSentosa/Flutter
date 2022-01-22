e57103a6dcb214ac742c36fe650a0bd7f2713d8f
PriyadiSentosa <priyadisentosa825@gmail.com>
2022-01-22 13:47:09 -0800

    looping

--- looping/latihan1.dart
+++ looping/latihan1.dart
@@ -1,1 +1,9 @@
-import "dart:io";
+import 'dart:io';
+
+void main() {
+  print("Masukan Angka : ");
+  int? nilai = int.parse(stdin.readLineSync()!);
+  for (var angka = 1; angka < nilai; angka++) {
+    print('Iterasi ke-' + angka.toString());
+  }
+}
